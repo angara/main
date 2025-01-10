3097c71d153a4ba6e335bf3c3fdbb9f47051f0b7
Maxim Penzin <maxp@ya.ru>
2025-01-11 00:37:09 +0800

    meteo: curr_temp fix

--- src/meteo/curr_temp.clj
+++ src/meteo/curr_temp.clj
@@ -1,11 +1,28 @@
 (ns meteo.curr-temp
   (:require
+    [java-time.api :as jt]
     [meteo.data :refer [active-stations]]
     [meteo.fmt :refer [format-t]]
    ,))
 
 
+(defn fresh-ts? [ts]
+  (when ts
+    (try
+      (jt/after? (jt/instant ts) 
+                 (jt/minus (jt/instant) (jt/minutes 80)))
+      (catch Exception _ nil)
+      )))
+
+
+(defn fresh-t [{last :last :as st-info}]
+  (when (and (:t last)
+             (fresh-ts? (:t_ts last)))
+    st-info))
+
+
 (defn curr-temp [_]
-  (when-let [{last :last} (first (active-stations))]
+  (when-let [{last :last} (some fresh-t (active-stations))]
     (when-let [t (format-t "" (:t last) (:t_delta last))]
       [:span.t t])))
+
